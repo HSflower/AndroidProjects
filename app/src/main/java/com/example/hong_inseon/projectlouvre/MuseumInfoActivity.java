bda5a8131e5b4a6beddeeb12b5f71c6f133bf62e
inseon0731 <inseon0731@naver.com>
2017-02-27 21:20:44 +0900

    커스텀 리스트뷰 완성, (기본)다이얼로그 완성.

--- app/src/main/java/com/example/hong_inseon/projectlouvre/MuseumInfoActivity.java
+++ app/src/main/java/com/example/hong_inseon/projectlouvre/MuseumInfoActivity.java
@@ -3,11 +3,11 @@ package com.example.hong_inseon.projectlouvre;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class GalleryInfoActivity extends AppCompatActivity {
+public class MuseumInfoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_gallery_info);
+        setContentView(R.layout.activity_museum_info);
     }
 }
