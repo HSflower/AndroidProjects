262cedd02a1386d85f8cdf669c6a6e79827e7808
pc\samsung <2983rgt@naver.com>
2017-02-27 23:19:54 +0900

    메인액티비티 수정

--- app/src/main/java/com/example/hong_inseon/projectlouvre/MainActivity.java
+++ app/src/main/java/com/example/hong_inseon/projectlouvre/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClick(View view) {
-        Intent intent = new Intent(this, ExhibitionInfoDrawer.class);
+        Intent intent = new Intent(this, ExhibitionInfoActivity.class);
         startActivity(intent);
     }
 
