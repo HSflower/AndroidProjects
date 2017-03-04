7d59662afec5a3c2bfb292825c3fa5b0aaa85e33
pc\samsung <2983rgt@naver.com>
2017-03-04 14:11:40 +0900

    server url edit

--- app/src/test/java/dao/ServerUtil.java
+++ app/src/test/java/dao/ServerUtil.java
@@ -1,6 +1,6 @@
 package dao;
 
 public class ServerUtil {
-	public static final String SERVER_URL = "http://192.168.0.3/webData/";
+	public static final String SERVER_URL = "http://35.161.181.60/";
 //	public static final String SERVER_URL = "http://192.168.202.146/webData/";
 }
