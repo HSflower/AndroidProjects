68f940b1442398b18627b0595e0939249c1ea8c3
pc\samsung <2983rgt@naver.com>
2017-06-07 04:57:18 +0900

    android projects merge agian

--- nullProject/Android/app/src/main/java/com/example/hong_inseon/projectlouvre/dao/Review.java
+++
@@ -1,8 +0,0 @@
-package com.example.hong_inseon.projectlouvre.dao;
-
-/**
- * Created by juneb on 2017-05-20.
- */
-
-public class Review {
-}
--- nullProject/Android/app/src/test/java/com/example/hong_inseon/projectlouvre/ListActivityTest.java
+++
@@ -1,8 +0,0 @@
-package com.example.hong_inseon.projectlouvre;
-
-/**
- * Created by Hong-Inseon on 2017. 2. 19..
- */
-public class ListActivityTest {
-
-}
